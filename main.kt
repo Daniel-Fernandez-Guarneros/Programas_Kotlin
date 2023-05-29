1c00b79e7d3c967688525af37cefc595c6e7eb8e
Daniel Fernandez Guarneros <danierse63@gmail.com>
2023-05-29 12:54:11 -0600

    1_1 Programa Hello World Main

---
+++ main.kt
@@ -0,0 +1,41 @@
+// Primer programa en Kotlin
+
+fun main() {
+    //Un val es un valor inmutable
+    val veces = 35;
+    val border =  "_*";
+
+    printBorder(1, 30);
+    println("Hola mundo Kotlin");
+    printBorder(5, 40);
+    println();
+    println("En este programa, el caracter border es: ${border}");
+    println("Y se imprime ${veces} veces");
+}
+
+// fun printBorder(numero: Int, caracter: String){
+//     repeat(numero){
+//         print(caracter);
+//     }
+//     println();
+// }
+
+fun printBorder(tipoCaracter: Int, numero: Int){
+    when (tipoCaracter){
+        1 ->
+            repeat(numero){
+                print("*");
+            }
+        2 ->
+            repeat(numero){
+                print("-");
+            }
+        3 ->
+            repeat(numero){
+                print("&");
+            }
+        else -> 
+            println("Tipo de caracter no valido, no se puede imprimir el borde");
+    }
+    println();
+}
