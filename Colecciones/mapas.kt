b55e6747870fd048d014938a2e042ef3e7fa48be
Daniel Fernandez Guarneros <danierse63@gmail.com>
2023-06-03 02:15:11 -0600

    5_2 Programa de mapas y Tarea de realizar un Diccionario Español - Ingles con un mapa

---
+++ Colecciones/mapas.kt
@@ -0,0 +1,153 @@
+// Trabajando con mapas
+
+fun main() {
+    // Mapa inmutable
+    val ladas: Map<Int, String> = mapOf(
+        1 to "USA",
+        34 to "España",
+        52 to "Mexico",
+        233 to "Ghana"
+    );
+
+    // Imprimiendo el conjunto de pares clave-valor
+    println(ladas);
+
+    // Imprimiendo el mapa usando un for
+    for ((key, value) in ladas){
+        println("La clave lada de $value es $key");
+    }
+
+    // Imprimiendo el mapa usando lambda
+    ladas.forEach{ (k, v) -> println("$k -> $v")}
+
+    // Recuperar un solo elemento del mapa
+    println(ladas.get(52));
+    println(ladas[52]);
+
+    // Recuperar su tamaño, ¿Cuántos pares hay en el mapa?
+    println("Tamaño: ${ladas.size}");
+    // Recuperar solo las claves
+    println("Claves del mapa: ${ladas.keys}");
+    // Recuperar solo los valores
+    println("Valores del mapa: ${ladas.values}");
+    // Recuperar las entradas
+    println("Entradas del mapa: ${ladas.entries}");
+    // Recuperar un valor si existe, si no, envia default
+    println("Valor de la lada 48: ${ladas.getOrDefault(48, "Pais desconocido")}");
+    // Comprobar si el mapa esta vacio
+    println("Mapa vacio: ${ladas.isEmpty()}");
+    // Comprobar si existe una clave especifica en el mapa
+    println("¿Existe la clave lada 86?: ${86 in ladas}");
+    // Comprobar si existe un valor en el mapa
+    println("¿Existe el valor Mexico en el mapa?: ${"Mexico" in ladas.values}");
+
+    // Mapa mutable
+    var nums = mutableMapOf(
+        "one" to 1,
+        "two" to 2,
+        "three" to 3,
+        "four" to 4
+    );
+
+    println("Mapa de numeros: $nums");
+    // Eliminar valores del mapa
+    nums.remove("two");
+    // Agregando par al mapa
+    nums["six"] = 6;
+    println("Mapa nuevo de numeros: $nums");
+
+    var sortedNums = nums.toSortedMap();
+    println("Mapa ordenado: $sortedNums");
+
+    // Reto:
+    // Crear un diccionario español - ingles usando un mapa
+    // 1. Funcion para crear o cargar el diccionario
+    // 2. Funcion para consulta, reciba una palabra en español y regrese su traduccion, si no existe, un valor por default
+    // 3. Imprimir todo el diccionario
+
+    var separador = "======================================================================";
+    println(separador);
+    println("Tarea de creacion de un diccionario Español - Ingles usando un mapa");
+    println(separador);
+
+    // 1. Funcion para crear o cargar el diccionario
+    val diccionario = crearDiccionario();
+    println("Se ha creado exitosamente el diccionario");
+    println(separador);
+
+    do {
+        println("Teclea 1 si deseas buscar una palabra para saber su significado de español a ingles");
+        println("Teclea 2 si deseas imprimir todo el diccionario");
+        println("Teclea 3 si desea terminar el programa");
+        var terminar = true;
+
+        try{
+            var choice = readLine()!!.toInt();
+            println(separador);
+            when(choice){
+                1 -> {
+                    // 2. Funcion para consulta, reciba una palabra en español y regrese su traduccion, si no existe, un valor por default
+                    println("Ingresa la palabra en español para saber su significado en ingles: ")
+                    var palabra = readLine()!!.toLowerCase();
+                    println("----------------------------------------------------------------------");
+                    consultarDiccionario(palabra, diccionario);
+                    println(separador);
+                }
+                2 -> {
+                    // 3. Imprimir todo el diccionario
+                    println("Todas las traducciones del diccionario son las siguientes: ");
+                    println("----------------------------------------------------------------------");
+                    imprimirDiccionario(diccionario);
+                    println(separador);
+                }
+                3 -> {
+                    terminar = false;
+                    println("Programa terminado correctamente!!");
+                    println(separador);
+                }
+            }
+        } catch (exception: NumberFormatException) {
+            println(separador);
+            println("Ha ocurrido un error, debes ingresar un numero entero");
+            println(separador);
+        }
+    } while (terminar);
+}
+
+// 1. Funcion para crear o cargar el diccionario
+fun crearDiccionario(): Map<String, String> {
+    var palabras = mutableMapOf(
+        "caja" to "box",
+        "manzana" to "apple",
+        "libro" to "book",
+        "celular" to "cellphone",
+        "teclado" to "keyboard",
+        "computadora" to "computer",
+        "carro" to "car",
+        "videojuego" to "videogame",
+        "consola" to "console",
+        "tortuga" to "turtle",
+        "tiburon" to "shark",
+        "lagrima" to "tear",
+        "odio" to "hate",
+        "vida" to "life",
+        "amor" to "love",
+        "dormir" to "sleep",
+        "caballero" to "knight",
+        "noche" to "night",
+        "luna" to "moon",
+        "oscuro" to "dark",
+        "sangre" to "blood"
+    );
+    return palabras;
+}
+
+// 2. Funcion para consulta, reciba una palabra en español y regrese su traduccion, si no existe, un valor por default
+fun consultarDiccionario(palabra: String, diccionario: Map<String, String>){
+    println("Traduccion de la palabra ${palabra}: ${diccionario.getOrDefault(palabra, "La palabra no existe en el diccionario")}");
+}
+
+// 3. Imprimir todo el diccionario
+fun imprimirDiccionario(diccionario: Map<String, String>){
+    diccionario.forEach{ (k, v) -> println("$k -> $v") };
+}
